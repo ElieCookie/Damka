28677f5624c67e35ad1ce6881575b9aaf63fd475
ElieCookie13 <eleanor@appcharge.com>
2023-06-09 19:51:22 +0300

    wip

--- q5.c
+++ q5.c
@@ -1,5 +1,7 @@
 #include "general_settings.h"
 
+// Params: a board
+// Prints the current status of the board
 void printBoardStatus(Board board){
     printf("+-+-+-+-+-+-+-+-+-+\n+ |1|2|3|4|5|6|7|8|\n+-+-+-+-+-+-+-+-+-+\n");
 
@@ -18,6 +20,8 @@ void printBoardStatus(Board board){
     }
 }
 
+// Params: a board, a player
+// according to the given player, it checks if there is a piece at the beginning of the opponents' side of the board
 bool checkIfLastRow(Board board, Player player){
     if(player == 'T'){
         for(int i = 0; i<BOARD_SIZE; i+=2){
@@ -35,6 +39,8 @@ bool checkIfLastRow(Board board, Player player){
     }
 }
 
+// Params: a board, a player
+// counts the existing pieces of a given player, and returns it
 int countPieces(Board board, Player player){
     int pieces_counter = 0;
 
@@ -47,7 +53,16 @@ int countPieces(Board board, Player player){
     return pieces_counter;
 }
 
-bool MakeTurn(Board board, Player p1, Player p2, PiecesNum *p1_pieces, PiecesNum *p2_pieces, int *p1_moves_count,
+// Params: a board, p1 - the player making the move, p2 - the opponent,
+// p2_pieces - a pointer to the number of pieces of the opponent,
+// p1_moves_count - a pointer to the number of moves of the player making the current move,
+// curr_p2_piece_count - pointer to the current number of the opponents piece count,
+// max_p1_captures_count - a pointer of the maximal number of captures per move that p1 made,
+// max_p2_captures_count - a pointer of the maximal number of captures per move that p2 made
+
+// The function updates the number of moves of p1, updates the current piece count of p2,
+// makes the turn and checks if the player won.
+bool MakeTurn(Board board, Player p1, Player p2, PiecesNum *p2_pieces, int *p1_moves_count,
               int *curr_p2_piece_count, int *max_p1_captures_count, int *max_p2_captures_count){
     printf("%c's turn:\n", p1);
     Turn(board, p1);
@@ -74,6 +89,9 @@ bool MakeTurn(Board board, Player p1, Player p2, PiecesNum *p1_pieces, PiecesNum
     return false;
 }
 
+// Params: a board, the starting player
+// The function initiates the piece counter of each player, determines the opponent,
+// and calls the turns of the players in a loop.
 void PlayGame(Board board, Player starting_player) {
 
     PiecesNum starting_pieces = 12;
@@ -97,7 +115,7 @@ void PlayGame(Board board, Player starting_player) {
 
     while (!starting_won || !opposing_won) {
 
-        starting_won = MakeTurn(board, starting_player, opposing_player, &starting_pieces,
+        starting_won = MakeTurn(board, starting_player, opposing_player,
                                 &opposing_pieces, &starting_player_moves_count,
                                 &curr_opposing_piece_count,
                                 &max_starting_player_captures_count,
@@ -106,7 +124,7 @@ void PlayGame(Board board, Player starting_player) {
         if(starting_won)
             break;
 
-        opposing_won = MakeTurn(board, opposing_player, starting_player, &opposing_pieces,
+        opposing_won = MakeTurn(board, opposing_player, starting_player,
                                 &starting_pieces, &opposing_player_moves_count,
                                 &curr_starting_piece_count,
                                 &max_opposing_player_captures_count,
