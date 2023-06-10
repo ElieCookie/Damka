2b80c89bb40efb77f11d7431c72996b54b6c927c
ElieCookie13 <eleanor@appcharge.com>
2023-06-10 22:40:58 +0300

    final changes

--- main.c
+++ main.c
@@ -9,8 +9,14 @@ int main(){
     // Assign players
     Player starting_player;
 
-    printf("Enter the player that will start the game. T for top player, B for bottom player: \n");
-    scanf("%c", &starting_player);
+    do {
+        printf("Enter the player that will start the game. T for top player, B for bottom player: \n");
+        scanf(" %c", &starting_player);
+
+        if (starting_player != 'T' && starting_player != 'B') {
+            printf("Invalid input. Please enter a valid player (T or B).\n");
+        }
+    } while (starting_player != 'T' && starting_player != 'B');
 
    PlayGame(board, starting_player);
    return 0;
